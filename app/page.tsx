53394b58b1b1f2d2f0084e5510d5a38351915f41
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-06-29 16:58:01 -0300

    feat: finished home section and h1 title animated

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,48 @@
+"use client";
+import { motion } from "framer-motion";
 import SocialLinks from "./components/SocialLinks";
 
 export default function Home() {
+
 	return (
-		<div className="flex items-center justify-center">
-			<div className='grid grid-rows-5 grid-cols-10 max-w-4xl'>
-				<div className="col-start-3 col-end-7 self-end">
-					<h2 className='font-extrabold text-2xl text-gray-700 tracking-tighter'>
-						OLÁ &#x1F44B; EU SOU O PEDRO
-					</h2>
+		<>
+			<section className="bg-hero-colorful bg-cover bg-no-repeat h-screen">
+				<div className="bg-[rgba(255,255,255,0.4)] h-full">
+					<div className='flex flex-col w-3/4 sm:w-3/5 md:w-[500px] lg:w-2/4 mx-auto pt-24'>
+						<div className="">
+							<h2 className='font-extrabold text-base sm:text-lg md:text-xl lg:text-2xl text-zinc-900 tracking-tighter text-left'>
+								OLÁ &#x1F44B; EU SOU O PEDRO
+							</h2>
+						</div>
+						<div className="flex items-center">
+							<motion.div
+								initial={{ opacity: 0, y: -15 }}
+								animate={{ opacity: 1, y: 0 }}
+							>
+								<h1 className='title bg-gradient-to-r from-[#007cf0] to-[#00dfd8] pr-3'>
+									Fullstack
+								</h1>
+							</motion.div>
+							<div className="pl-3">
+								<SocialLinks />
+							</div>
+						</div>
+						<motion.div
+							initial={{ opacity: 0, y: -15 }}
+							animate={{ opacity: 1, y: 0 }}
+						>
+							<h1 className='title bg-gradient-to-r from-[#ff4d4d] to-[#f9cb28] second pb-3'>
+								Developer
+							</h1>
+						</motion.div>
+						<div className="">
+							<h3 className='text-zinc-900 text-[10px] sm:text-xs md:font-normal md:text-sm lg:font-medium text-right'>
+								Estudante de Sistemas de Informação, tenho 21 anos. Amo estudar tecnologia e descobrir coisas novas. Tenho experiência com desenvolvimento fullstack a mais de dois anos. No momento estudando mais sobre Nest.JS ...
+							</h3>
+						</div>
+					</div>
 				</div>
-				<div className='row-start-2 col-start-3 col-end-8 self-end'>
-					<h1 className='title bg-gradient-to-r from-[#7928ca] to-[#ff0080]'>
-						Fullstack
-					</h1>
-				</div>
-				<div className="row-start-2 col-start-8 self-center">
-					<SocialLinks />
-				</div>
-				<div className="row-start-3 col-start-3 col-end-9 self-start">
-					<h1 className='title bg-gradient-to-r from-[#ff4d4d] to-[#f9cb28] second pb-3'>
-						Developer
-					</h1>
-				</div>
-				<div className="row-start-4 col-start-4 col-end-10 flex-wrap">
-					<h3 className='max-w-lg text-end mt-6 ml-44 text-gray-700 font-medium'>
-						Estudante de Sistemas de Informação, tenho 21 anos. Amo estudar tecnologia e descobrir coisas novas. Tenho experiência com desenvolvimento fullstack a mais de dois anos. No momento estudando mais sobre Nest.JS ...
-					</h3>
-				</div>
-			</div>
-		</div>
+			</section>
+		</>
 	)
 }
