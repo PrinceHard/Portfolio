fbefa443361788f22f265023d7c94fa3976043ed
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-04 14:01:18 -0300

    feat: added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes:{
+        gradient: {
+          '0%': {backgroundPosition: '0% 50%'},
+          '50%': {backgroundPosition: '100% 50%'},
+          '100%': {backgroundPosition: '0% 50%'}
+        }
+      },
+      animation:{
+        gradient: 'gradient 15s ease infinite'
+      },
       fontFamily:{
         inter: ['var(--font-inter)'],
+        plaster: ['var(--font-plaster)']
       },
       backgroundImage: {
         'hero-colorful': "url('http://localhost:3000/main-gradient.png')",
