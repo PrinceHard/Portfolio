14eebe59f48c3cc300a9629f7c8186083be31417
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-04 14:01:49 -0300

    feat: created data mock

---
+++ app/data/index.ts
@@ -0,0 +1,16 @@
+const URL = "http://localhost:3000/"
+
+export const listObject = [
+    {
+        src: "https://sekailist-five.vercel.app/",
+        title: "Site de Animes",
+        mockupImage: `${URL}mockup-sekailist-notebook.png`,
+        technologies: [`${URL}icons8-nativo-de-reagir-48.png`, `${URL}icons8-javascript-48.png`]
+    },
+    {
+        src: "https://cristmas-app-store.netlify.app/",
+        title: "Landing Page Natalina",
+        mockupImage: `${URL}mockup-site-natal.png`,
+        technologies: [`${URL}icons8-html-48.png`, `${URL}icons8-css-48.png`, `${URL}icons8-javascript-48.png`]
+    }
+]
