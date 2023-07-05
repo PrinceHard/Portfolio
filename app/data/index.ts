cfaafbc9ee3da62e22e3b31a9591db963fc115a0
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-05 18:46:15 -0300

    fix: added id propertie for the key value

--- app/data/index.ts
+++ app/data/index.ts
@@ -2,12 +2,14 @@ const URL = "http://localhost:3000/"
 
 export const listObject = [
     {
+        id: 1,
         src: "https://sekailist-five.vercel.app/",
         title: "Site de Animes",
         mockupImage: `${URL}mockup-sekailist-notebook.png`,
         technologies: [`${URL}icons8-nativo-de-reagir-48.png`, `${URL}icons8-javascript-48.png`]
     },
     {
+        id: 2,
         src: "https://cristmas-app-store.netlify.app/",
         title: "Landing Page Natalina",
         mockupImage: `${URL}mockup-site-natal.png`,
