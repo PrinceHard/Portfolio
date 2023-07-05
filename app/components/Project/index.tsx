6f8427d24c0c294cb264162208bbd8f9ebb68e37
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-05 18:35:10 -0300

    feat: responsiveness of project component

--- app/components/Project/index.tsx
+++ app/components/Project/index.tsx
@@ -12,10 +12,10 @@ const Project = ({ project }: TypeProps) => {
     return (
         <a href={project.src} target="_blank">
             <div className="flex flex-col items-center gap-1">
-                <div className="w-96 h-56">
+                <div className="w-7/12 sm:w-60 sm:h-40 md:w-64 lg:w-80 lg:h-48 big:w-96 big:h-56">
                     <img className="h-full" src={project.mockupImage} alt="mockup-image" />
                 </div>
-                <h2 className="text-2xl font-semibold text-white tracking-tighter uppercase">
+                <h2 className="text-lg sm:text-lg md:text-lg lg:text-xl big:text-2xl font-semibold text-white tracking-tighter uppercase">
                     {project.title}
                 </h2>
                 <div className="flex justify-center gap-3 h-8">
