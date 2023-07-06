c7dde097b6855365d474c73052386a88dd2a48c5
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-06 02:14:25 -0300

    fix: changed path and added width from next/image

--- app/components/SocialLinks/index.tsx
+++ app/components/SocialLinks/index.tsx
@@ -1,19 +1,16 @@
 import Image from "next/image";
-import linkedinIcon from '../../../public/linkedin.png'
-import githubIcon from '../../../public/github.png'
-import fileIcon from '../../../public/document.png'
 
 const SocialLinks = () => {
     return (
         <div className='flex gap-1'>
             <a href='https://br.linkedin.com/in/pedro-henrique-89689b146' target='_blank' className='hover:-translate-y-1.5 transition-transform duration-100'>
-                <Image src={linkedinIcon} alt='linkedin-link' width={35} />
+                <Image src="/linkedin.png" alt='linkedin-link' width={35} height={35} />
             </a>
             <a href='https://github.com/PrinceHard/' target='_blank' className='hover:-translate-y-1.5 transition-transform duration-100'>
-                <Image src={githubIcon} alt='github-link' width={35} />
+                <Image src='/github.png' alt='github-link' width={35} height={35} />
             </a>
             <a href='http://localhost:3000/Resume.pdf' download target='_blank' className='hover:-translate-y-1.5 transition-transform duration-100'>
-                <Image src={fileIcon} alt='curriculum-link' width={35} />
+                <Image src='/document.png' alt='curriculum-link' width={35} height={35} />
             </a>
         </div>
     )
