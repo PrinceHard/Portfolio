13e8f33a1719f4278050c9ebbfcba180f922e2fb
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-07-06 02:13:35 -0300

    fix: change path of resources

--- app/data/index.ts
+++ app/data/index.ts
@@ -1,18 +1,16 @@
-const URL = "http://localhost:3000/"
-
 export const listObject = [
     {
         id: 1,
         src: "https://sekailist-five.vercel.app/",
         title: "Site de Animes",
-        mockupImage: `${URL}mockup-sekailist-notebook.png`,
-        technologies: [`${URL}icons8-nativo-de-reagir-48.png`, `${URL}icons8-javascript-48.png`]
+        mockupImage: "/mockup-sekailist-notebook.png",
+        technologies: ["/react.png", "/javascript.png"]
     },
     {
         id: 2,
         src: "https://cristmas-app-store.netlify.app/",
         title: "Landing Page Natalina",
-        mockupImage: `${URL}mockup-site-natal.png`,
-        technologies: [`${URL}icons8-html-48.png`, `${URL}icons8-css-48.png`, `${URL}icons8-javascript-48.png`]
+        mockupImage: "/mockup-site-natal.png",
+        technologies: ["/html.png", "/css.png", "/javascript.png"]
     }
 ]
